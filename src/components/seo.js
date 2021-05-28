19ad485b7bd9d4c2b506d22d10af14427535b48d
Caleb Barnes <caleb.d.barnes@gmail.com>
2021-05-28 00:25:51 -0700

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,8 +6,6 @@ import Parser from "html-react-parser"
 const Seo = props => {
   const { title, lang = "en", fullHead } = props
 
-  console.log({ props })
-
   return (
     <Helmet
       htmlAttributes={{
