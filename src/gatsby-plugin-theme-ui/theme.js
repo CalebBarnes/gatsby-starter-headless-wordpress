5f824fc015f54ba25c6469220adec68e53d88e7f
Caleb Barnes <caleb.d.barnes@gmail.com>
2021-05-28 01:13:20 -0700

    Update theme.js

--- src/gatsby-plugin-theme-ui/theme.js
+++ src/gatsby-plugin-theme-ui/theme.js
@@ -4,6 +4,7 @@ import { deep, system } from "@theme-ui/presets"
  * ? For more info about Theming see https://theme-ui.com/theming
  * ? For more presets see https://theme-ui.com/packages/presets/
  */
+
 const theme = {
   ...deep,
   styles: {
